025efccb3dd5f4c339c288084c441531d9a7445e
mesutdmn <mesutduman@outlook.com>
2024-10-31 00:25:45 +0300

    new-language

--- graph.py
+++ graph.py
@@ -15,10 +15,8 @@ class LLMs:
                                             You are an exam preparation expert tasked with generating multiple-choice questions from the provided context. For each question:
                                             - Generate **exactly four options**, and label them as "A", "B", "C", and "D".
                                             - Only one option should be correct. Indicate this by setting only one option as `True` in the answers list, with the others as `False`.
-                                            Although this instruction is in English, provide the final output in the specified language.
-                                            \n\nTranslate to: {language}
-                                            
-                                            \n\nProvide the response in the specified JSON structure. **Do not translate specific terms, labels, or the JSON structure**:
+
+                                            Provide the response in the specified JSON structure. **Do not translate specific terms, labels, or the JSON structure**:
                                             {{
                                                 "question": "The text of the question.",
                                                 "choices": [
@@ -32,10 +30,11 @@ class LLMs:
                                                 "explain": "A brief explanation of why the correct answer is correct."
                                             }}
 
-                                            
-                                            \nContext: {context}
-                                            
-                                            \n{format_instructions}
+                                            Although this instruction is in English, provide the final output in the specified language.
+
+                                            \n\nContext: {context}
+                                            \n\nOutput Language: {language}
+                                            \n\n{format_instructions}
                                         """
 
     def question_maker(self, input):
